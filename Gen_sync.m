0f50743b3f5a8a4517ba62481bf04de3a507ec7b
unknown <kyriakos@accusonus.com>
2021-01-21 18:31:21 +0200

    more comments, code styling fixes and .gitignore

--- Gen_sync.m
+++ Gen_sync.m
@@ -22,114 +22,105 @@ close all;
 delete(instrfind);
 disp('Starting the Application . . . . ');
 
-ecg_data = importdata('ecg_1.txt'); 
-Fs = 5000;
-ReceivedData = [];                % Array that contains the received data downsampled to 500hz.
-qrs = [];                         % Array that contains the indexes of the qrs complex in the ReceivedData array.
+FREQUENCY = 5000;
+
+dataToTransmit = importdata('ecg_1.txt');
+receivedDataArray = [];           % Array that contains the received data downsampled to 500hz.
+qrsArray = [];                    % Array that contains the indexes of the qrs complex in the ReceivedData array.
 RRIntervalArray = [];             % Array that contains the time difference (RRInterval) between each qrs complex and its successive qrs complex in seconds.
-FiveMinsMeanArray = [];           % Array that contains the mean values of the RRIntervals for all five minute segments of the signal.
-FiveMinsDeviationArray = [];      % Array that contains the standard diviation values of the RRIntervals for all five minute segments of the signal.
-SuccessiveIntervalArray = [];     % Array that contains the difference betwen each RRInterval and its successive RRInterval.
+fiveMinsMeanArray = [];           % Array that contains the mean values of the RRIntervals for all five minute segments of the signal.
+fiveMinsDeviationArray = [];      % Array that contains the standard diviation values of the RRIntervals for all five minute segments of the signal.
+successiveIntervalArray = [];     % Array that contains the difference betwen each RRInterval and its successive RRInterval.
 NN50 = 0;                         % The number of RR intervals differing by > 50ms from the preceding interval.
-[ReceivedData, qrs, RRIntervalArray, FiveMinsMeanArray, FiveMinsDeviationArray] = start(...
-  ecg_data, Fs, ReceivedData, qrs, RRIntervalArray, FiveMinsMeanArray, FiveMinsDeviationArray);
+[receivedDataArray, qrsArray, RRIntervalArray, fiveMinsMeanArray, fiveMinsDeviationArray] = start(...
+  dataToTransmit, FREQUENCY, receivedDataArray, qrsArray, RRIntervalArray, fiveMinsMeanArray, fiveMinsDeviationArray);
 
 for i=2:1:length(RRIntervalArray)
-  SuccessiveInterval = RRIntervalArray(i) - RRIntervalArray(i - 1);
-  SuccessiveIntervalArray = [SuccessiveIntervalArray SuccessiveInterval];
+  successiveInterval = RRIntervalArray(i) - RRIntervalArray(i - 1);
+  successiveIntervalArray = [successiveIntervalArray successiveInterval];
   
-  if abs(SuccessiveInterval)*1000 > 50
+  if abs(successiveInterval)*1000 > 50
     NN50 = NN50 + 1;
   end
 end
 
 SDNN = std(RRIntervalArray);                     % Standard deviation of all normal to normal RR intervals
-SDANN = std(FiveMinsMeanArray);                  % Standard deviation of 5-minute average RR intervals
-ASDNN = mean(FiveMinsDeviationArray);            % Mean of the standard deviations of all RR intervals fow all 5 minute segments
-rMSSD = sqrt(mean(SuccessiveIntervalArray.^2));  % Square root of the mean of the squares of successive RR interval differences
+SDANN = std(fiveMinsMeanArray);                  % Standard deviation of 5-minute average RR intervals
+ASDNN = mean(fiveMinsDeviationArray);            % Mean of the standard deviations of all RR intervals fow all 5 minute segments
+rMSSD = sqrt(mean(successiveIntervalArray.^2));  % Square root of the mean of the squares of successive RR interval differences
 pNN50 = (NN50/length(RRIntervalArray))*100;      % The perventage of RR intervals differing by > 50ms from the preceding interval
 
-function [DataArray, qrs, RRIntervalArray, FiveMinsMeanArray, FiveMinsDeviationArray] = start(...
-  ecg_data, Fs, DataArray, qrs, RRIntervalArray, FiveMinsMeanArray, FiveMinsDeviationArray)
+function [receivedDataArray, qrsArray, RRIntervalArray, fiveMinsMeanArray, fiveMinsDeviationArray] = start(...
+  dataToTransmit, FREQUENCY, receivedDataArray, qrsArray, RRIntervalArray, fiveMinsMeanArray, fiveMinsDeviationArray)
   %% Initialise variables
-  %Nc=length(ecg_data);       % amount of character to send
-  Nc = 1570000;
-  packet = 1000;              % size of each packet
-  s1OutpuBuffer = 2000;       % Tx buffer length
-  s2InputBuffer = 2000;       % Rx buffer length
-  disptime = 0.1;             % Time interval between displaying in graph in seconds
-  DownsampleFs = 500;         % We are downsampling data to that frequency
-                              % before sending them to pan_tomkins
-  qrswindow = 2*DownsampleFs; % A sufficient amount of data (2 seconds) in order to check for QRS
-  PowStep = DownsampleFs/10;  % The window in which we check for possible QRS complexes by 
-                              % computing the power of the signal. It should be 100ms.
+  %Nc=length(dataToTransmit);       % amount of character to send
+  nValuesToSend = 1570000;
+  FRAME_LENGTH = 1000;              % size of each frame
+  S1_OUTPUT_BUFFER_LENGTH = 2000;   % Tx buffer length
+  S2_INPUT_BUFFER_LENGTH = 2000;    % Rx buffer length
+  BAUD_RATE = 115200;
+  DISP_TIME = 0.1;                  % Time interval between displaying in graph in seconds
+  DOWNSAMPLE_FREQUENCY = 500;       % We are downsampling data to that frequency
+                                    % before sending them to pan_tomkins
+  QRS_WINDOW = 2*DOWNSAMPLE_FREQUENCY; % A sufficient amount of data (2 seconds) in order to check for QRS
+  POW_STEP = DOWNSAMPLE_FREQUENCY/10;  % The window in which we check for possible QRS complexes by 
+                                       % computing the power of the signal. It should be 100ms.
 
-  fiveMinWindowLength = DownsampleFs*60*5; % The length of a five minute segment measured in data samples
-  fiveMeanWindowIndex = 1;                 % The index where the previous five minute segment ends.
-  fiveMinWindowCnt = 1;                    % Counter of five minute segments
+  fiveMinWindowLength = DOWNSAMPLE_FREQUENCY*60*5; % The length of a five minute segment measured in data samples
+  previousFiveMinWindowIndex = 1;                  % The index where the previous five minute segment ends.
+  fiveMinWindowCnt = 1;                            % Counter of five minute segments
   
   %% Open ports
   disp('Opening RS232 ports . . . . . ');
-  s1 = serial('COM3','BaudRate',115200, 'OutputBufferSize', s1OutpuBuffer);
-  s2 = serial('COM4','BaudRate',115200,'InputBufferSize', s2InputBuffer);
+  s1 = serial('COM3','BaudRate',BAUD_RATE, 'OutputBufferSize', S1_OUTPUT_BUFFER_LENGTH);
+  s2 = serial('COM4','BaudRate',BAUD_RATE,'InputBufferSize', S2_INPUT_BUFFER_LENGTH);
   s2.BytesAvailableFcnMode = 'byte';
   s2.BytesAvailableFcnCount = 2000;
-  s2.BytesAvailableFcn = @(src, ~)onBytesAvailable(src, @pan_tompkin, DownsampleFs, qrswindow, PowStep);
+  s2.BytesAvailableFcn = @(src, ~)onBytesAvailable(src, @pan_tompkin, DOWNSAMPLE_FREQUENCY, QRS_WINDOW, POW_STEP);
   fopen(s1); fopen(s2); 
   
   disp('RS232 receiver and transmitter ports activated');
   disp(' ');
   
   %% Set up plot
-  figure('NumberTitle','off',...
-      'Name','Electrocardiography',...
-      'Color',[0 0 0]);
-  plotHandle = plot(0,'Marker','.',...
-    'LineWidth',1,'Color',[0.1328    0.5430    0.1328]);
-  axis([0 10*DownsampleFs -600 600]);
-
-  set(gca,...
-      'XColor', [0.9375 1 1],...
-      'YColor', [0.9375 1 1]);
-
-  xlabel('Data','FontWeight','bold','FontSize',12,...
-    'Color',[0.8516    0.6445    0.1250]);
-  ylabel('Amplitude','FontWeight','bold','FontSize',12,...
-    'Color',[0.8516    0.6445    0.1250]);
-  title('Real Time Data','FontSize',12,...
-    'Color',[0.8516    0.6445    0.1250]);
+  figure('NumberTitle','off', 'Name','Electrocardiography', 'Color',[0 0 0]);
+  plotHandle = plot(0,'Marker','.', 'LineWidth',1, 'Color',[0.1328 0.5430 0.1328]);
+  axis([0 10*DOWNSAMPLE_FREQUENCY -600 600]);
+  set(gca, 'XColor',[0.9375 1 1], 'YColor',[0.9375 1 1]);
+  xlabel('Data', 'FontWeight','bold', 'FontSize',12, 'Color',[0.8516 0.6445 0.1250]);
+  ylabel('Amplitude', 'FontWeight','bold', 'FontSize',12, 'Color',[0.8516 0.6445 0.1250]);
+  title('Real Time Data', 'FontSize',12, 'Color',[0.8516 0.6445 0.1250]);
   %% Start sending data
+  REPEAT_SEND = 3;
   times = 1;
-  cnt = 0;
-  disptic=tic;
-  while Nc - cnt > 0
-    if times > 10
+  valuesSentCnt = 0;
+  disptic = tic;
+  while nValuesToSend - valuesSentCnt > 0
+    if times > REPEAT_SEND
       break;
     end
     if ~s1.BytesToOutput
       tic;
-      if Nc - cnt >= packet
-        fwrite(s1,ecg_data(cnt+1:cnt+packet),'int16');
+      if nValuesToSend - valuesSentCnt >= FRAME_LENGTH
+        fwrite(s1, dataToTransmit(valuesSentCnt + 1:valuesSentCnt + FRAME_LENGTH), 'int16');
       else
-        fwrite(s1,ecg_data(cnt+1:Nc),'int16');
+        fwrite(s1, dataToTransmit(valuesSentCnt + 1:nValuesToSend), 'int16');
       end
-      cnt = cnt + packet;
+      valuesSentCnt = valuesSentCnt + FRAME_LENGTH;
       
-      if cnt == Nc
-        cnt = 0;
+      if valuesSentCnt == nValuesToSend
+        valuesSentCnt = 0;
         times = times + 1;
       end
-      %while toc<pdelay
-      %end
     end
-    if toc(disptic)>disptime
-      if length(DataArray)<=10*DownsampleFs
-        set(plotHandle,'YData',DataArray); % draw all
+    if toc(disptic) > DISP_TIME
+      if length(receivedDataArray) <= 10*DOWNSAMPLE_FREQUENCY
+        set(plotHandle, 'YData', receivedDataArray); % draw all
       else
-        set(plotHandle,'YData',DataArray(end-10*DownsampleFs +1:end)); % draw last 10 seconds
+        set(plotHandle, 'YData', receivedDataArray(end-10*DOWNSAMPLE_FREQUENCY + 1:end)); % draw last 10 seconds
       end
       snapnow;
-      disptic=tic;
+      disptic = tic;
     end
   end
 
@@ -144,37 +135,37 @@ function [DataArray, qrs, RRIntervalArray, FiveMinsMeanArray, FiveMinsDeviationA
   disp('RS232 ports deactivated');
   disp(' ');
   %% Callback to receive and process data
-  function onBytesAvailable(src, pan_tompkin, DownsampleFs, qrswindow, PowStep)
+  function onBytesAvailable(src, pan_tompkin, DOWNSAMPLE_FREQUENCY, QRS_WINDOW, POW_STEP)
     if ~src.BytesAvailable
       return
     end
-    SamplesToReadFromPort=floor(src.BytesAvailable/2);
-    ReadBuffer = fread(src, SamplesToReadFromPort, 'int16')';
-    ReadBuffer = downsample(ReadBuffer, Fs/DownsampleFs); % Downsample to 500Hz 
-    DataArray = [DataArray ReadBuffer]; % Append received values to previously received data
+    nSamplesToReadFromPort = floor(src.BytesAvailable/2);
+    readBufferArray = fread(src, nSamplesToReadFromPort, 'int16')';
+    readBufferArray = downsample(readBufferArray, FREQUENCY/DOWNSAMPLE_FREQUENCY); % Downsample to 500Hz 
+    receivedDataArray = [receivedDataArray readBufferArray]; % Append received values to previously received data
     fprintf('\nCharacters received: %d\n', src.ValuesReceived);
     
-    if length(DataArray) < 1.2*qrswindow % We need at least 2 seconds of information to run pan_tomkins
+    if length(receivedDataArray) < 1.2*QRS_WINDOW % We need at least 2 seconds of information to run pan_tomkins
       return
     end
     
-    for i=1:PowStep:length(ReadBuffer) % Iterate on the newly received data with 100ms step
-      currentDataIndex = length(DataArray) - length(ReadBuffer) + i;
-      if isQRSPossible(DataArray(currentDataIndex - PowStep:currentDataIndex))
+    for i=1:POW_STEP:length(readBufferArray) % Iterate on the newly received data with 100ms step
+      currentDataIndex = length(receivedDataArray) - length(readBufferArray) + i;
+      if isQRSPossible(receivedDataArray(currentDataIndex - POW_STEP:currentDataIndex))
         % We provide the last 2 seconds of data to pan_tompkin
-        samlplesToProcess = DataArray(currentDataIndex - qrswindow:currentDataIndex);
-        [~,qrs_i_raw]=pan_tompkin(samlplesToProcess, DownsampleFs, 0);
-        qrs = getQRSIndexInDataArray(qrs, qrs_i_raw, currentDataIndex, qrswindow, PowStep, DownsampleFs);
+        samlplesToProcessArray = receivedDataArray(currentDataIndex - QRS_WINDOW:currentDataIndex);
+        [~,qrs_i_raw] = pan_tompkin(samlplesToProcessArray, DOWNSAMPLE_FREQUENCY, 0);
+        qrsArray = getQRSIndexInReceivedDataArray(qrsArray, qrs_i_raw, currentDataIndex, QRS_WINDOW, POW_STEP, DOWNSAMPLE_FREQUENCY);
       end
       
       if currentDataIndex - fiveMinWindowCnt*fiveMinWindowLength >= 0
-        FiveMinsMean=mean(RRIntervalArray(fiveMeanWindowIndex:end));
-        FiveMinsMeanArray=[FiveMinsMeanArray FiveMinsMean];
+        FiveMinsMean=mean(RRIntervalArray(previousFiveMinWindowIndex:end));
+        fiveMinsMeanArray=[fiveMinsMeanArray FiveMinsMean];
         
-        FiveMinsDeviation=std(RRIntervalArray(fiveMeanWindowIndex:end));
-        FiveMinsDeviationArray=[FiveMinsDeviationArray FiveMinsDeviation];
+        FiveMinsDeviation = std(RRIntervalArray(previousFiveMinWindowIndex:end));
+        fiveMinsDeviationArray = [fiveMinsDeviationArray FiveMinsDeviation];
         
-        fiveMeanWindowIndex = length(RRIntervalArray) + 1;
+        previousFiveMinWindowIndex = length(RRIntervalArray) + 1;
         fiveMinWindowCnt = fiveMinWindowCnt + 1;
       end
     end
@@ -184,27 +175,27 @@ function [DataArray, qrs, RRIntervalArray, FiveMinsMeanArray, FiveMinsDeviationA
       output = rms(data)^2 > 10000;
     end
     
-    function [qrs] = getQRSIndexInDataArray(qrs, qrs_i_raw, currentDataIndex, qrswindow, PowStep, DownsampleFs)
+    function [qrsArray] = getQRSIndexInReceivedDataArray(qrsArray, qrs_i_raw, currentDataIndex, QRS_WINDOW, POW_STEP, DOWNSAMPLE_FREQUENCY)
       % Gets the current qrs array and adds the newly found indexes.
-      % The indexes should represent the right position on the DataArray.
+      % The indexes should represent the right position on the receivedDataArray.
  
       % if it is the first time we return all found indexes
-      if isempty(qrs)
-        qrs = qrs_i_raw + currentDataIndex - qrswindow;
-        for j=1:1:length(qrs)-1
-          RRIntervalArray(j)=(qrs(j+1)-qrs(j))/DownsampleFs;
+      if isempty(qrsArray)
+        qrsArray = qrs_i_raw + currentDataIndex - QRS_WINDOW;
+        for j=1:1:length(qrsArray)-1
+          RRIntervalArray(j)=(qrsArray(j+1)-qrsArray(j))/DOWNSAMPLE_FREQUENCY;
         end
         return 
       end
 
-      % Transform qrs index to the corresponding DataArray index
-      qrs_i_raw = qrs_i_raw + currentDataIndex - qrswindow;
+      % Transform qrsArray index to the corresponding receivedDataArray index
+      qrs_i_raw = qrs_i_raw + currentDataIndex - QRS_WINDOW;
       % make sure this is a new qrs by comparing its value
       % with the latest found qrs
-      if abs(qrs_i_raw(end)-qrs(end)) > PowStep
-        qrs(end + 1) = qrs_i_raw(end); % append the last found qrs to the ones we have
-        RRInterval = (qrs(end)-qrs(end - 1))/DownsampleFs;
-        RRIntervalArray = [ RRIntervalArray RRInterval ];
+      if abs(qrs_i_raw(end)-qrsArray(end)) > POW_STEP
+        qrsArray(end + 1) = qrs_i_raw(end); % append the last found qrs to the ones we have
+        RRInterval = (qrsArray(end)-qrsArray(end - 1))/DOWNSAMPLE_FREQUENCY;
+        RRIntervalArray = [RRIntervalArray RRInterval];
       end
     end
   end
